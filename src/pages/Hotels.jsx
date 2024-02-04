703dcbfda363c5641da28db54e1f79c1c2a5c43a
Chamika Dilshan <chamidilshan55@gmail.com>
2024-02-04 17:59:51 +0530

    added hotel table view and popup

--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PermanentDrawerLeft from '../components/drawer'
 import Box from '@mui/material/Box';
+import { Button, Checkbox, Dialog, DialogContent, DialogTitle, FormControlLabel, IconButton, Paper, Radio, RadioGroup, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TextField } from "@mui/material";
 
 
 export const Hotels = () => {
+
+  const columns = [
+    {id: '_id', name: 'ID'},
+    {id: 'hotelName', name: 'Name'},
+    {id: 'hotelDistrict', name: 'District'},
+    {id: 'hotelImage', name: 'Image'},
+    {id: 'action', name: 'Actions'},
+  ]
+
+  const [_id, idChange] = useState(0);
+  const [hotelName, hotelNameChange] = useState('');
+  const [hotelDistrict, hotelDistrictChange] = useState('');
+  const [hotelImage, hotelImageChange] = useState('');
+  const [open, openChange] = useState(false);
+
+  const functionAdd = ()=>{
+    openPopUp();
+  }
+
+  const closePopUp = ()=>{
+    openChange(false);
+  }
+
+  const openPopUp = ()=>{ 
+    openChange(true);
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const _obj= {_id, hotelName, hotelDistrict, hotelImage};
+    console.log(_obj);
+  }
+
+  
+
   return (
     <>
     <Box sx={{ display: 'flex' }}>
@@ -11,6 +47,45 @@ export const Hotels = () => {
      <h1 className='text-cyan-900'>Hotels</h1>
     </Box>
      
+     <Paper sx={{margin: '1%'}}>
+      <div className='flex justify-end'>
+        <Button onClick={functionAdd} variant="contained" color="primary">Add New Hotel</Button>
+      </div>
+      <div style={{margin: '1%'}}>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow style={{backgroundColor: '#D4D4D4'}}>
+            {columns.map((column) => (
+              <TableCell key={column.id}>
+                {column.name}
+              </TableCell>
+            ))}
+            </TableRow> 
+          </TableHead>
+          <TableBody>
+            
+          </TableBody>
+        </Table>
+      </TableContainer> 
+      </div>
+     </Paper>
+
+     <Dialog open={open} onClose={closePopUp} fullWidth maxWidth='sm' >
+      <DialogTitle>
+        <span>Create A Hotel</span>
+      </DialogTitle>
+      <DialogContent>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={2} margin={2}>
+            <TextField value={hotelName} required onChange={e=>{hotelNameChange(e.target.value)}} variant='outlined' label='Hotel Name'></TextField>
+            <TextField value={hotelDistrict} onChange={e=>{hotelDistrictChange(e.target.value)}} variant='outlined' label='Hotel District'></TextField>
+            <TextField value={hotelImage} onChange={e=>{hotelImageChange(e.target.value)}} variant='outlined' label='Hotel Image'></TextField>
+            <Button type='submit' variant='contained'>Save</Button>
+          </Stack>
+        </form>
+      </DialogContent>
+     </Dialog>
    </>
   )
 }
